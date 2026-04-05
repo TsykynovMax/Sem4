a96bb5771c529baf32417114c1b427e8d4e42531
Цыкунов Максим <enelit1719@gmail.com>
2026-04-05 14:53:46 +0300

    Task3

---
+++ main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <deque>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::deque<int> D = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 }; 
+
+    int N = D.size();
+
+    std::cout << "»сходный дек: ";
+    for (int x : D) std::cout << x << " ";
+    std::cout << std::endl;
+
+    auto i = D.begin();
+
+    for (int k = 0; k < N / 4; ++k) {
+        i = D.erase(++i);
+    }
+
+    std::cout << "ƒек после удалени¤: ";
+    for (int x : D) std::cout << x << " ";
+    std::cout << std::endl;
+
+    return 0;
+}
