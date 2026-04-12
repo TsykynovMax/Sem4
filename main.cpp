56b305f6fce44dcd749be85b629e5946b3a15218
Цыкунов Максим <enelit1719@gmail.com>
2026-04-12 23:24:41 +0300

    Task4 правки

--- main.cpp
+++ main.cpp
@@ -1,124 +1,40 @@
 ﻿#include <iostream>
-#include <fstream>
-#include <vector>
-#include <algorithm>
-#include <functional>
+#include <deque>
 #include <string>
+#include <sstream>
 #include <iterator>
-#include <clocale>
 
-using namespace std::placeholders;
-
-struct point
+void printDeque(const std::deque<int>& D, const std::string& message)
 {
-    int x;
-    int y;
-    std::string s;
-
-    point() : x(0), y(0), s("") {}
-
-    point(int x_, int y_, const std::string& s_) : x(x_), y(y_), s(s_) {}
-
-    bool operator<(const point& other) const
-    {
-        if (x != other.x) return x < other.x;
-        return y < other.y;
-    }
-
-    point mult(int k) const
-    {
-        point result;
-        result.x = k * x;
-        result.y = k * y;
-        result.s = s;
-        return result;
-    }
-
-    point operator+(const point& other) const
-    {
-        point result;
-        result.x = x + other.x;
-        result.y = y + other.y;
-        result.s = s;
-        return result;
-    }
-};
-
-std::istream& operator>>(std::istream& is, point& p)
-{
-    is >> p.x >> p.y >> p.s;
-    return is;
-}
-
-std::ostream& operator<<(std::ostream& os, const point& p)
-{
-    os << p.x << " " << p.y << " " << p.s;
-    return os;
+    std::cout << message;
+    for (int x : D) std::cout << x << " ";
+    std::cout << std::endl;
 }
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
 
-    int K = 0;
-    std::string name1 = "";
-    std::string name2 = "";
+    std::deque<int> D;
+    std::string line;
 
-    std::cout << "Введите число K: ";
-    std::cin >> K;
-    std::cout << "Введите имя первого файла: ";
-    std::cin >> name1;
-    std::cout << "Введите имя второго файла: ";
-    std::cin >> name2;
+    std::cout << "Введите элементы дека: ";
+    std::getline(std::cin, line);
+    std::stringstream ss(line);
 
-    std::ifstream file1(name1);
-    if (!file1.is_open())
-    {
-        std::cout << "Ошибка: не удалось открыть файл " << name1 << std::endl;
-        return 1;
+    std::copy(std::istream_iterator<int>(ss), std::istream_iterator<int>(), std::back_inserter(D));
+
+    int N = D.size();
+
+    printDeque(D, "Исходный дек: ");
+
+    auto i = D.begin();
+
+    for (int k = 0; k < N / 4; ++k) {
+        i = D.erase(++i);
     }
 
-    std::vector<point> V1;
-    std::copy(
-        std::istream_iterator<point>(file1),
-        std::istream_iterator<point>(),
-        std::back_inserter(V1)
-    );
-    file1.close();
-
-    std::ifstream file2(name2);
-    if (!file2.is_open())
-    {
-        std::cout << "Ошибка: не удалось открыть файл " << name2 << std::endl;
-        return 1;
-    }
-
-    std::vector<point> V2;
-    std::copy(
-        std::istream_iterator<point>(file2),
-        std::istream_iterator<point>(),
-        std::back_inserter(V2)
-    );
-    file2.close();
-
-    if (V1.size() != V2.size())
-    {
-        std::cout << "Ошибка: файлы содержат разное количество элементов!" << std::endl;
-        std::cout << "V1: " << V1.size() << ", V2: " << V2.size() << std::endl;
-        return 1;
-    }
-
-    std::vector<point> result(V1.size());
-
-    for (size_t i = 0; i < V1.size(); ++i)
-    {
-        result[i] = V1[i].mult(K) + V2[i];
-    }
-
-    std::cout << "\nРезультат преобразований:\n";
-    for (const auto& p : result) {
-        std::cout << p << std::endl;
-    }
+    printDeque(D, "Дек после удаления: ");
 
     return 0;
 }
